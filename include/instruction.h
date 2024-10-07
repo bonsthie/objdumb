fc5520cc83de4994a0cde4527f1ad265a2eadfbb
bonsthie <barnabe.bonnet@gmail.com>
2024-10-07 19:43:21 +0200

    add of bytecode value

--- include/instruction.h
+++ include/instruction.h
@@ -1,15 +1,18 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
+#include <stdint.h>
+
 typedef struct	s_instruction
 {
 	char					*name;
+	uint64_t				bitcode;
 	char					*instr;
 	char					*desc;
 	struct s_instruction	*next;
 }	t_instruction;
 
-#define INSTRUCTION_INPUT(enum_value, value, definition) enum_value,
+#define INSTRUCTION_INPUT(enum_value, bitcode, value, definition) enum_value = bitcode,
 
 enum e_instruction
 {
@@ -18,7 +21,7 @@ enum e_instruction
 
 #undef INSTRUCTION_INPUT
 
-#define INSTRUCTION_INPUT(enum_value, value, definition) [enum_value] = value,
+#define INSTRUCTION_INPUT(enum_value, bitcode, value, definition) [enum_value] = value,
 
 /* ODR crampte */
 char *instruction_table[] = {
@@ -27,10 +30,12 @@ char *instruction_table[] = {
 
 #undef INSTRUCTION_INPUT
 
-#define INSTRUCTION_INPUT(enum_value, value, definition) [enum_value] = definition,
+#define INSTRUCTION_INPUT(enum_value, bitcode, value, definition) [enum_value] = definition,
 char *instruction_definition[] = {
 #include "instruction_tmp.h"
 };
 
 
 #endif /* INSTRUCTION_H */
+
+
